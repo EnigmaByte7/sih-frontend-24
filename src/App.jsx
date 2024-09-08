3e5ac841baa1ef88c7818199b85af987b1ffbc20
Yash Saxena <134928189+EnigmaByte7@users.noreply.github.com>
2024-09-08 13:39:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ import About from './pages/projects/pulsar/about';
 import HomePage from './pages/home';
 import Talk from './pages/Talk';
 import MainAbout from './pages/MainAbout';
-import  Projects from './pages/projects/projects';
-import Classify from './pages/projects/pulsar/classify';
-import Discuss from './pages/projects/pulsar/discuss';
-import Collection from './pages/projects/pulsar/collection';
+import  projects from './pages/projects/projects';
+import classify from './pages/projects/pulsar/classify';
+import discuss from './pages/projects/pulsar/discuss';
+import collection from './pages/projects/pulsar/collection';
 import UnderConstruction from './pages/underconstruction';
 import ExpandedView from './pages/projects/pulsar/ExpandedView';
 
@@ -27,10 +27,10 @@ export default function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path="/projects/pulsar" element={<PulsarPage/>}></Route>
-        <Route path="/projects" element={<Projects/>}></Route>
+        <Route path="/projects" element={<projects/>}></Route>
         <Route path="/projects/pulsar/about" element={<About />} />
-        <Route path="/projects/pulsar/classify" element={<Classify />} />
-        <Route path="/projects/pulsar/collection" element={<Collection />} />
+        <Route path="/projects/pulsar/classify" element={<classify />} />
+        <Route path="/projects/pulsar/collection" element={<collection />} />
         <Route path="/projects/pulsar/discuss" element={<Talk/>} />
         <Route path="/about" element={<MainAbout/>} />
         <Route path="/talk" element={<Talk/>} />
